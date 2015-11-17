8dcff2bb95c402d2536a8315eda6f34063eca3fd
IssacCZ <sheldowkay@icloud.com>
2015-11-17 09:38:13 +0800

    Show and Hide Keyboard

--- MyLocations/Codes/LocaitonDetailsVC.swift
+++ MyLocations/Codes/LocaitonDetailsVC.swift
@@ -53,6 +53,20 @@ class LocationDetailsVC: UITableViewController {
         }
         
         dateLabel.text = formatDate(NSDate())
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("hideKeyboard:"))
+        gestureRecognizer.cancelsTouchesInView = false
+        tableView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    func hideKeyboard(gestureRecognizer: UIGestureRecognizer) {
+        let point = gestureRecognizer.locationInView(tableView)
+        let indexPath = tableView.indexPathForRowAtPoint(point)
+        if indexPath != nil && indexPath!.section == 0 && indexPath!.row == 0 {
+            return
+        }
+        
+        descriptionTextView.resignFirstResponder()
     }
     
     func stringFromPlacemark(placemark: CLPlacemark) -> String {
@@ -109,5 +123,20 @@ class LocationDetailsVC: UITableViewController {
         let controller = segue.sourceViewController as! CategoryPickerVC
         categoryName = controller.selectedCategoryName
         categoryLabel.text = categoryName
+    }
+    
+    // MARK: - UITableViewDelegate
+    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        if indexPath.section == 0 || indexPath.section == 1 {
+            return indexPath
+        } else {
+            return nil
+        }
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            descriptionTextView.becomeFirstResponder()
+        }
     }
 }
