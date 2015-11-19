e48f94cb74c0e3afb94f02a2da390c90032022f5
IssacCZ <sheldowkay@icloud.com>
2015-11-19 08:58:22 +0800

    Adding Core Data

---
+++ MyLocations/Codes/Location.swift
@@ -0,0 +1,17 @@
+//
+//  Location.swift
+//  MyLocations
+//
+//  Created by IssacCZ on 11/19/15.
+//  Copyright © 2015 Issac. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Location: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
